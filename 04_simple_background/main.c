c495a34a0cda9c3634e63d0b195afa5f99b3ca34
rajathc <rajath2665@gmail.com>
2024-09-03 16:33:53 +0530

    add 04_simple_background

---
+++ 04_simple_background/main.c
@@ -0,0 +1,17 @@
+#include <gb/gb.h>
+#include "simplebackground.c"
+#include "simplebackgroundmap.c"
+
+void main(){
+    set_bkg_data(0, 7, backgroundtiles);
+    set_bkg_tiles(0, 0, 40, 18, backgroundmap);
+
+    SHOW_BKG;
+    DISPLAY_ON;
+
+    while (1){
+        scroll_bkg(1,0);
+        wait_vbl_done();
+    }
+    
+}
